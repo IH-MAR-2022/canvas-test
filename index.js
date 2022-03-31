f8053eee8f4e161d93d2c391da7152c795fc8e0b
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-31 14:11:18 -0400

    push

--- index.js
+++ index.js
@@ -56,38 +56,135 @@ const w = canvas.width;
 // ctx.closePath();
 
 let square = {
-  x: 10,
-  y: 10,
+  x: 435,
+  y: 447,
   w: 25,
   h: 25,
   xReverse: false,
   yReverse: false,
+  speed: 1,
 };
 
+//SCREEN SAVER
+
 // ctx.fillRect(square.x, square.y, square.w, square.h);
+
+// function animate() {
+//   window.requestAnimationFrame(animate);
+//   ctx.clearRect(0, 0, w, h);
+//   ctx.fillRect(square.x, square.y, square.w, square.h);
+
+//   //Collision Detection
+//   //left wall
+//   if (square.x + square.w >= w) {
+//     square.speed++;
+//     square.xReverse = true;
+//   }
+//   //bottom wall
+//   if (square.y + square.h >= h) {
+//     square.speed++;
+//     square.yReverse = true;
+//   }
+//   //Right wall
+//   if (square.x <= 0) {
+//     square.speed++;
+//     square.xReverse = false;
+//   }
+//   //Top wall
+//   if (square.y <= 0) {
+//     square.speed++;
+//     square.yReverse = false;
+//   }
+
+//   if (square.yReverse === false) {
+//     square.y += square.speed * 0.75;
+//   } else {
+//     square.y -= square.speed * 0.75;
+//   }
+//   if (square.xReverse === false) {
+//     square.x += square.speed;
+//   } else {
+//     square.x -= square.speed;
+//   }
+// }
+
+//This is how many pixels our character will move
+let movementSpeed = 10;
+
+//EVENT LISTENER
+
+function move(e) {
+  switch (e.key) {
+    case "ArrowUp":
+      if (square.y - movementSpeed < 0) {
+        square.y = 0;
+      } else {
+        square.y -= movementSpeed;
+      }
+      break;
+    case "ArrowDown":
+      if (square.y + square.h + movementSpeed > h) {
+        square.y = h - square.h;
+      } else {
+        square.y += movementSpeed;
+      }
+      break;
+    case "ArrowLeft":
+      if (square.x - movementSpeed < 0) {
+        square.x = 0;
+      } else {
+        square.x -= movementSpeed;
+      }
+      break;
+    case "ArrowRight":
+      if (square.x + square.w + movementSpeed > w) {
+        square.x = w - square.w;
+      } else {
+        square.x += movementSpeed;
+      }
+      break;
+  }
+}
+
+document.addEventListener("keydown", move);
 
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.clearRect(0, 0, w, h);
   ctx.fillRect(square.x, square.y, square.w, square.h);
 
-  if (square.x + square.w >= w) {
-    square.xReverse = true;
-  }
-  if (square.y + square.h >= h) {
-    square.yReverse = true;
-  }
+  // //Collision Detection
+  // //left wall
+  // if (square.x + square.w >= w) {
+  //   square.speed++;
+  //   square.xReverse = true;
+  // }
+  // //bottom wall
+  // if (square.y + square.h >= h) {
+  //   square.speed++;
+  //   square.yReverse = true;
+  // }
+  // //Right wall
+  // if (square.x <= 0) {
+  //   square.speed++;
+  //   square.xReverse = false;
+  // }
+  // //Top wall
+  // if (square.y <= 0) {
+  //   square.speed++;
+  //   square.yReverse = false;
+  // }
 
-  if (square.yReverse === false) {
-    square.y++;
-  } else {
-    square.y--;
-  }
-  if (square.xReverse === false) {
-    square.x++;
-  } else {
-    square.x--;
-  }
+  // if (square.yReverse === false) {
+  //   square.y += square.speed * 0.75;
+  // } else {
+  //   square.y -= square.speed * 0.75;
+  // }
+  // if (square.xReverse === false) {
+  //   square.x += square.speed;
+  // } else {
+  //   square.x -= square.speed;
+  // }
 }
 
 animate();
